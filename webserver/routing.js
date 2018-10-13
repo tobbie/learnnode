3e0a37933d3c1c303cf2880ecd91d1fc0497dea4
tobbie <tobi.akinseye@gmail.com>
2018-10-13 12:47:36 +0100

    node as webserver

---
+++ webserver/routing.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+var fs = require('fs');
+
+http.createServer(function(req, res){
+
+    if(req.url === '/')
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+     var html = fs.readFileSync(__dirname +'/index.html', 'utf8');
+     var message = 'sample text';
+    html = html.replace('{message}', message);
+     res.end(html);
+
+    }
+    else if(req.url === '/api'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var obj = {
+            firstname : 'Sam',
+            lastname : 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+    else{
+        res.writeHead(404);
+        res.end()
+    }
+     
+}).listen(1338, '127.0.0.1');
---
+++ webserver/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var fs = require('fs');
+
+http.createServer(function(req, res){
+
+     res.writeHead(200, {'Content-Type': 'text/html'});
+     var html = fs.readFileSync(__dirname +'/index.html', 'utf8');
+     var message = 'sample text';
+    html = html.replace('{message}', message);
+     res.end(html);
+
+}).listen(1338, '127.0.0.1');
